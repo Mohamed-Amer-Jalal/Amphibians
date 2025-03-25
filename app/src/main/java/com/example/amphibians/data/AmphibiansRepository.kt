6f360b5be52b3b08f1fc1ace0bd34ba215a9b440
M-Amer <mohamedamerjalal@gmail.com>
2025-03-25 22:59:07 +0200

    feat: Add AmphibiansRepository interface and implementation
    - Introduce `AmphibiansRepository` interface in `AmphibiansRepository.kt` to define a contract for retrieving amphibian data.
    - Implement `DefaultAmphibiansRepository` in `AmphibiansRepository.kt` as a network-based implementation of the repository, using `AmphibiansApiService` to fetch amphibian data.

---
+++ app/src/main/java/com/example/amphibians/data/AmphibiansRepository.kt
@@ -0,0 +1,17 @@
+package com.example.amphibians.data
+
+import com.example.amphibians.model.Amphibian
+import com.example.amphibians.network.AmphibiansApiService
+
+/** Repository retrieves amphibian data from underlying data source. */
+interface AmphibiansRepository {
+    /** Retrieves list of amphibians from underlying data source */
+    suspend fun getAmphibians(): List<Amphibian>
+}
+
+/** Network Implementation of repository that retrieves amphibian data from underlying data source. */
+class DefaultAmphibiansRepository(val amphibianApiService: AmphibiansApiService) :
+    AmphibiansRepository {
+    /** Retrieves list of amphibians from underlying data source */
+    override suspend fun getAmphibians(): List<Amphibian> = amphibianApiService.getAmphibians()
+}
